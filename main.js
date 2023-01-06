56b1e23cebdba824f4c1867f566c1316e84640aa
italoyuri <italoyuriv.sousa@gmail.com>
2023-01-05 23:43:36 -0300

    fim

--- main.js
+++ main.js
@@ -1,16 +1,32 @@
-function tocaSom(idElementoAudio){
-    return document.querySelector(idElementoAudio).play();
+function tocaSom(idElementoAudio) {
+    const elemento = document.querySelector(idElementoAudio);
+    if (elemento === null){
+        console.log("Elemento não encontrado")
+    } else {
+        return elemento.play();
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-for  (let i = 0; i < listaDeTeclas.length; i++) {
+for (let i = 0; i < listaDeTeclas.length; i++) {
 
     const tecla = listaDeTeclas[i];
 
-    const idAudio = `#som_${tecla.classList[1]}`
+    const idAudio = `#som_${tecla.classList[1]}`;
 
     tecla.onclick = function () {
         tocaSom(idAudio)
     };
+
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Enter' || evento.code === 'Space') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 }
